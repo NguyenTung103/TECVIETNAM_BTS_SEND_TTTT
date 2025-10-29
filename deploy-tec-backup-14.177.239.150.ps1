1cdbcaafd635b1c5e5d4e8b958328ca6a16a8dca
PinoNTT <tientung@qig.vn>
2025-10-29 22:21:38 +0700

    f

--- deploy-tec-backup-14.177.239.150.ps1
+++ deploy-tec-backup-14.177.239.150.ps1
@@ -1,4 +1,4 @@
-. .\shell-common.ps1
+. "$PSScriptRoot\shell-command.ps1"
 param([string]$buildLocation, [string]$webRunFolder, [string]$appSettingJson, [string]$serviceName, [string]$applicationExe)
 $ErrorActionPreference = "Stop"
 DeployServices $buildLocation $webRunFolder $appSettingJson $serviceName $applicationExe
