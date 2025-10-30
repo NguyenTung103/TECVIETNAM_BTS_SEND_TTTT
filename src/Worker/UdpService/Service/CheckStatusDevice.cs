189d9f616261d61892d937229ae8d70bb64b5f57
PinoNTT <tientung@qig.vn>
2025-10-30 08:41:24 +0700

    cập nhật hàm status

--- src/Worker/UdpService/Service/CheckStatusDevice.cs
+++ src/Worker/UdpService/Service/CheckStatusDevice.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Udp;
+﻿using Core.PushMessage;
+using Infrastructure.Udp;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -14,8 +15,18 @@ namespace UdpService.Service
     public class CheckStatusDevice : BackgroundService
     {
         private readonly IUdpService _udpService;
+        private readonly IPushMessageService _pushMessageService;
+        public CheckStatusDevice(
+            IUdpService udpService,
+            IPushMessageService pushMessageService
+            )
+        {
+            _udpService = udpService;
+            _pushMessageService = pushMessageService;
+        }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await _pushMessageService.SendMessageAsync("Check status device is started.");
             // Run the cleanup task every hour (adjust as needed)
             while (!stoppingToken.IsCancellationRequested)
             {
